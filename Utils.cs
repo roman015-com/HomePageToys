80b44bf5ed722581d82255433302a3691ac9bf73
roman015 <xaviervarricatt@live.in>
2022-03-26 15:10:09 +0800

    Added Extension Method

---
+++ Utils.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Roman015API.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePageToys
+{
+    public static class Utils
+    {
+        public static void AddHomePageToys(this IServiceCollection ServiceCollection)
+        {
+            ServiceCollection.AddSingleton<StarWarsHub>(sp => {
+                return StarWarsHub.GetSingletonInstance();
+            });
+        }
+    }
+}
